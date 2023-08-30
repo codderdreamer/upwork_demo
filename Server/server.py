bc6fe13c569daf3a1047ae84e04e4c5f8307b200
codderdreamer <sevdayucedagg@gmail.com>
2023-08-31 00:09:17 +0300

    dene

--- Server/server.py
+++ Server/server.py
@@ -9,8 +9,10 @@ async def handler(websocket, path):
         data = await websocket.recv()
         await websocket.send(data)
         if data == "open kivy interface":
+            os.system("sudo systemctl restart splashscreen.service")
             os.system("sudo python /home/pi/upwork_demo/Interface/kivy_interface_main.py")
         elif data == "open web interface":
+            os.system("sudo systemctl restart splashscreen.service")
             os.system("sudo su -l pi -c startx")
     except Exception as e:
         print(e)
