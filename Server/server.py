2480a541b85718eb01e4a72acdcf202e18fb5c44
codderdreamer <sevdayucedagg@gmail.com>
2023-08-30 23:06:46 +0300

    dene

--- Server/server.py
+++ Server/server.py
@@ -5,12 +5,15 @@ import time
 import os
  
 async def handler(websocket, path):
-    data = await websocket.recv()
-    await websocket.send(data)
-    if data == "open kivy interface":
-        os.system("sudo python /home/pi/upwork_demo/kivy_interface_main.py")
-    elif data == "open web interface":
-        os.system("sudo su -l pi -c startx")
+    try:
+        data = await websocket.recv()
+        await websocket.send(data)
+        if data == "open kivy interface":
+            os.system("sudo python /home/pi/upwork_demo/KeyboardClient/kivy_interface_main.py")
+        elif data == "open web interface":
+            os.system("sudo su -l pi -c startx")
+    except Exception as e:
+        print(e)
 
  
 start_server = websockets.serve(handler, "localhost", 8000)
